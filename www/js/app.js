b657823e6a104195fcb0796a4c6c98ad40efaa09
aaron <462826@qq.com>
2015-09-14 17:33:54 +0800

    init

--- www/js/app.js
+++ www/js/app.js
@@ -23,7 +23,8 @@ angular.module('mobistore', ['ionic', 'mobistore.controllers', 'mobistore.servic
     });
   })
 
-  .config(function ($stateProvider, $urlRouterProvider) {
+  .config(function ($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
+    $ionicConfigProvider.platform.android.tabs.position('bottom');
 
     // Ionic uses AngularUI Router which uses the concept of states
     // Learn more here: https://github.com/angular-ui/ui-router
