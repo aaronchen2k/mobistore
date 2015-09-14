2832b82370b387c241e261e5216247e95b70c155
aaron <462826@qq.com>
2015-09-14 16:55:20 +0800

    init

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,7 +1,12 @@
 angular.module('mobistore.controllers', [])
 
   .controller('HomeCtrl', function($scope) {
-
+    $scope.productions = [
+      {'id':'001','name': '如实无添加发酵乳', 'capacity': 135},
+      {'id':'002','name': '嘟嘟儿童高品质牛奶', 'capacity': 200},
+      {'id':'003','name': '新鲜杯优倍', 'capacity': 200},
+      {'id':'004','name': '白雪风味酸牛奶（低脂）', 'capacity': 200},
+    ]
   })
 
   .controller('CategoryCtrl', function($scope) {
