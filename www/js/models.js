734f6e91bacdd41191164e0274e51acf845e8de0
aaron <462826@qq.com>
2015-09-17 09:51:06 +0800

    tt

--- www/js/models.js
+++ www/js/models.js
@@ -3,11 +3,11 @@
 angular.module('mobistore.models', [])
 
 .factory('ProductMdl', ['$resource', 'Constant', function($resource, Constant){
-    return $resource(Constant.ApiPath + 'product/model/:id', {id:'@id'}, {});
+    return $resource(Constant.ApiPath + 'test/model/:id', {id:'@id'}, {});
 }])
 
 .factory('ProductOpt', ['$resource', 'Constant', function($resource, Constant){
-    return $resource(Constant.ApiPath + 'product/opt/:act', {act:'@act'}, {
+    return $resource(Constant.ApiPath + 'test/opt/:act', {act:'@act'}, {
         'opt': {method:'POST'}
     });
 }]);
