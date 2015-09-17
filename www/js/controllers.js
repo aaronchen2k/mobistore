343fa91e888b717c8d12bc0ab3221cfa58390586
aaron <462826@qq.com>
2015-09-17 15:56:43 +0800

    tt

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,47 +2,52 @@
 
 angular.module('mobistore.controllers', [])
 
-  .controller('TabCtrl', ['$scope', 'Util', function($scope, Util) {
+  .controller('TabCtrl', ['$scope', 'Util', 'HomeOpt', function($scope, Util, HomeOpt) {
 	  $scope.menuShow = false;
       $scope.showMenu = function() {
           $scope.menuShow = !$scope.menuShow;
           console.log($scope.menuShow);
       };
+      
+    HomeOpt.opt({act: 'index'},function(json) {
+    	console.log(json);
+    	$scope.categories = json.data;
+  	});
   }])
 
-  .controller('HomeCtrl', ['$scope', 'Util', 'ProductMdl', 'ProductOpt', function($scope, Util, ProductMdl, ProductOpt) {
+  .controller('HomeCtrl', ['$scope', 'Util', 'HomeOpt', 'ProductMdl', 'ProductOpt', function($scope, Util, HomeOpt, ProductMdl, ProductOpt) {
 	    $scope.platform = ionic.Platform.platform();
 	    var width = Util.getScreenSize().w;
 	    var height = width * 0.32;
 	    $scope.styleSlideHeight = {'height':height + 'px'};
 	    
-	    // 查找
-	    ProductMdl.get({ id: '1'}).$promise.then(function(p1) {
-	        console.log(p1);
-	        
-	        // 新增
-	        p1.id = '';
-	        ProductMdl.save(p1).$promise.then(function(p2) {
-	        	console.log(p2);
-	        	
-	        	// 更新
-	        	p2.name += '=';
-	        	ProductMdl.save(p2).$promise.then(function(p3) {
-		        	console.log(p3);
-		        	
-		        	// 查询
-		        	ProductMdl.query({'startIndex': 0},function (ls) {
-		      	      console.log(ls);
-		      	      	
-		      	      	// 信息
-			      	  	ProductOpt.opt({act: 'doSomething', 'param': 'test'},function(json) {
-			      	  		console.log(json);
-				      	});
-		      	  	});
-		        });
-	        });
-	        
-	    });
+//	    // 查找
+//	    ProductMdl.get({ id: '1'}).$promise.then(function(p1) {
+//	        console.log(p1);
+//	        
+//	        // 新增
+//	        p1.id = '';
+//	        ProductMdl.save(p1).$promise.then(function(p2) {
+//	        	console.log(p2);
+//	        	
+//	        	// 更新
+//	        	p2.name += '=';
+//	        	ProductMdl.save(p2).$promise.then(function(p3) {
+//		        	console.log(p3);
+//		        	
+//		        	// 查询
+//		        	ProductMdl.query({'startIndex': 0},function (ls) {
+//		      	      console.log(ls);
+//		      	      	
+//		      	      	// 信息
+//			      	  	ProductOpt.opt({act: 'doSomething', 'param': 'test'},function(json) {
+//			      	  		console.log(json);
+//				      	});
+//		      	  	});
+//		        });
+//	        });
+//	        
+//	    });
 
   }])
 
--- www/js/models.js
+++ www/js/models.js
@@ -2,6 +2,12 @@
 
 angular.module('mobistore.models', [])
 
+.factory('HomeOpt', ['$resource', 'Constant', function($resource, Constant){
+    return $resource(Constant.ApiPath + 'home/opt/:act', {act:'@act'}, {
+        'opt': {method:'POST'}
+    });
+}])
+
 .factory('ProductMdl', ['$resource', 'Constant', function($resource, Constant){
     return $resource(Constant.ApiPath + 'test/model/:id', {id:'@id'}, {});
 }])
